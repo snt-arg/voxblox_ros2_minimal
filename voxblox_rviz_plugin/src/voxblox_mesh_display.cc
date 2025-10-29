f59726df120550dde485a27b685d070d6e9b3903
miferco97 <miferco97@gmail.com>
2025-10-29 14:09:13 +0000

    [feat] migration complete, there are some lacking dependencies updloaded

--- voxblox_rviz_plugin/src/voxblox_mesh_display.cc
+++ voxblox_rviz_plugin/src/voxblox_mesh_display.cc
@@ -3,10 +3,9 @@
 #include <OGRE/OgreSceneManager.h>
 #include <OGRE/OgreSceneNode.h>
 
-#include <tf/transform_listener.h>
-
-#include <rviz/frame_manager.h>
-#include <rviz/visualization_manager.h>
+#include <rviz_common/frame_manager_iface.hpp>
+#include <rviz_common/logging.hpp>
+#include <rviz_rendering/objects/object.hpp>
 
 namespace voxblox_rviz_plugin {
 
@@ -22,16 +21,17 @@ void VoxbloxMeshDisplay::reset() {
 }
 
 void VoxbloxMeshDisplay::processMessage(
-    const voxblox_msgs::Mesh::ConstPtr& msg) {
-  // Here we call the rviz::FrameManager to get the transform from the
-  // fixed frame to the frame in the header of this Imu message.  If
+    voxblox_msgs::msg::Mesh::ConstSharedPtr msg) {
+  // Here we call the rviz FrameManager to get the transform from the
+  // fixed frame to the frame in the header of this message.  If
   // it fails, we can't do anything else so we return.
   Ogre::Quaternion orientation;
   Ogre::Vector3 position;
   if (!context_->getFrameManager()->getTransform(
           msg->header.frame_id, msg->header.stamp, position, orientation)) {
-    ROS_DEBUG("Error transforming from frame '%s' to frame '%s'",
-              msg->header.frame_id.c_str(), qPrintable(fixed_frame_));
+    RVIZ_COMMON_LOG_DEBUG_STREAM("Error transforming from frame '"
+                                   << msg->header.frame_id << "' to frame '"
+                                   << qPrintable(fixed_frame_) << "'");
     return;
   }
 
@@ -48,5 +48,5 @@ void VoxbloxMeshDisplay::processMessage(
 
 }  // namespace voxblox_rviz_plugin
 
-#include <pluginlib/class_list_macros.h>
-PLUGINLIB_EXPORT_CLASS(voxblox_rviz_plugin::VoxbloxMeshDisplay, rviz::Display)
+#include <pluginlib/class_list_macros.hpp>
+PLUGINLIB_EXPORT_CLASS(voxblox_rviz_plugin::VoxbloxMeshDisplay, rviz_common::Display)
